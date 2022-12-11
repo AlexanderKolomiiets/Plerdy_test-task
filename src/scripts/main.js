9b596f108999aedf4c3beb89be75b6a60db86726
Sasha Kolomiiets <kinbean444@gmail.com>
2022-12-12 00:44:08 +0200

    reasons section

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,17 @@
 'use strict';
+
+import Swiper, { Pagination, Navigation } from 'swiper';
+import 'swiper/swiper-bundle.min.css';
+
+new Swiper('.swiper', {
+  modules: [Navigation],
+
+  grabCursor: true,
+  rewind: true,
+  slidesPerView: 4.2,
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
